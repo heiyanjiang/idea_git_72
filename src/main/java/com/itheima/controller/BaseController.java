859cb970115d1000ca90b8ebd53aef52c886d70b
hongyu <1194441707@qq.com>
2018-12-01 20:48:46 +0800

    左侧新建动作类

--- src/main/java/com/itheima/controller/BaseController.java
+++ src/main/java/com/itheima/controller/BaseController.java
@@ -4,6 +4,7 @@ public class BaseController {
     //测试修改的文件
     public static void main(String[] args) {
         System.out.println("rrrrr");
+        System.out.println("bbbbb");
         System.out.println("llllll");
     }
 }
